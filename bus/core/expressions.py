fade3cea2c748cf4c5482d98d657b0ee8a0cd6ee
Frappe <max@san-home.com>
2016-09-17 14:27:06 -0700

    Improvements in expressions

--- bus/core/expressions.py
+++ bus/core/expressions.py
@@ -1,36 +1,61 @@
 #-*- coding: utf-8 -*-
 
-import numbers
-import operator
 
-binary_ops = (
-    ('Eq', '=', '__eq__'),
-    ('Ne', '!=', '__ne__'),
-    ('Lt', '<', '__lt__'),
-    ('Gt', '>', '__gt__'),
-    ('Le', '<=', '__le__'),
-    ('Ge', '>=', '__ge__'),
-    ('And', 'and', '__and__'),
-    ('Or', 'or', '__or__'),
-    ('Add', '+', '__add__'),
-    ('Sub', '-', '__sub__'),
-    ('Mul', '*', '__mul__'),
-    ('Div', '/', '__div__'),
-    ('Mod', '%', '__mod__'),
-    ('In', 'in', '__contains__'),
+# Given x instance of Expr and y anything
+logical_ops = (
+    ('Eq', '==', '__eq__'),  # x == y
+    ('Ne', '!=', '__ne__'), # x != y
+    ('Lt', '<', '__lt__'), # x < y
+    ('Gt', '>', '__gt__'), # x > y
+    ('Le', '<=', '__le__'), # x <= y
+    ('Ge', '>=', '__ge__'), # x >= y
+    ('And', '&', '__and__'), # x & y
+    ('RAnd', '&', '__rand__'), # y & x
+    ('Or', '|', '__or__'), # x | y
+    ('ROr', '|', '__or__'), # y | x
+    ('In', 'in', '__contains__'), # y in x
+)
+
+prefix_unary_logical_ops = (
+    ('Not', 'not', '__not__'), # if not x
+    ('Truth', 'truth', 'truth') # if x
+)
+
+infix_ops = (
+    ('Add', '+', '__add__'), # x + y
+    ('Iadd', '+=', '__iadd__'), # x += y
+    ('RAdd', '+', '__radd__'), # y + x
+    ('Concat', '+', '__concat__'), # str(x) + str(y)
+    ('Sub', '-', '__sub__'), # x - y
+    ('ISub', '-=', '__isub__'), # x -= y
+    ('RSub', '-', '__rsub__'), # y - x
+    ('Mul', '*', '__mul__'), # x * y
+    ('IMul', '*=', '__imul__'), # x *= y
+    ('RMul', '*', '__rmul__'), # y * x
+    ('Div', '/', '__div__'), # x / y
+    ('IDiv', '/=', '__idiv__'), # x /= y
+    ('RDiv', '/', '__rdiv__'), # y / x
+    ('Mod', '%', '__mod__'), # x % y
+    ('RMod', '%', '__rmod__'), # y % x
+    ('IMod', '%=', '__imod__'), # x %= y
+    ('Pow', '**', '__pow__'), # x ** y
+    ('RPow', '**', '__rpow__'), # y ** x
+    ('IPow', '**', '__ipow__') # x **= y
 )
 
 prefix_unary_ops = (
-    ('Not', 'not', '__invert__'),
-    ('Pos', '+', '__pos__'),
-    ('Neg', '-', '__neg__'),
+    ('Not', 'not', '__invert__'), # ~x
+    ('Pos', '+', '__pos__'), # +x
+    ('Neg', '-', '__neg__'), # -y
 )
 
 postfix_unary_ops = (
-    ('Is', 'is_', 'is_'),
-    ('IsNone', 'is_not', 'is_not')
+    ('Is', 'is_', 'is_'), # x is y
+    ('IsNot', 'is_not', 'is_not') # x is not y
 )
 
+binary_ops = logical_ops + infix_ops
+unary_ops = prefix_unary_logical_ops + prefix_unary_ops + postfix_unary_ops
 all_ops = prefix_unary_ops + postfix_unary_ops + binary_ops
 
 class Expr(object):
